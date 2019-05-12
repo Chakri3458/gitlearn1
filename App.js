aa9665aadee1815721a39071634a56bc1f9cb115
unknown <chakri3458@gmail.com>
2019-05-12 19:25:23 -0400

    first commit

---
+++ App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>{this.props.headingText}</h1>
+        <h2>{this.props.content}</h2>
+        
+      </div>
+    );
+  }
+}
+
+export default App;
